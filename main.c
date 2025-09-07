00c512642060603892e7e82418a8b6c3f7ae838e
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-07 08:27:26 +0800

    PracticeA - Welcome to AtCoder

---
+++ main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+  int a;
+  int b;
+  int c;
+  char s[101];
+  scanf("%d", &a);
+  scanf("%d %d", &b, &c);
+  scanf("%s", s);
+  printf("%d %s", a + b + c, s);
+  return 0;
+}
