857c4c5550d1031f4cb3ddf978d2f3fb7edba140
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-08 07:49:41 +0800

    PracticeA - Welcome to AtCoder - Rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    let mut buffer = String::new();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let a: u32 = buffer.trim().parse().unwrap();
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let b_c: Vec<u32> = buffer
+        .split_whitespace()
+        .map(|c| c.parse().unwrap())
+        .collect();
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let s = buffer.trim();
+    println!("{}", a + b_c[0] + b_c[1]);
+    println!("{}", s);
 }
