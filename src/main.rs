f502fd5a1422552f312bfac19c22748a13c565f5
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-08 08:35:29 +0800

    ABC086A - Product - Rust

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,15 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
-    let a: u32 = buffer.trim().parse().unwrap();
-    buffer.clear();
-    io::stdin().read_line(&mut buffer).unwrap();
     let b_c: Vec<u32> = buffer
         .split_whitespace()
         .map(|c| c.parse().unwrap())
         .collect();
-    buffer.clear();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let s = buffer.trim();
-    println!("{}", a + b_c[0] + b_c[1]);
-    println!("{}", s);
+    let b = b_c[0];
+    let c = b_c[1];
+    if b * c % 2 == 0 {
+        println!("Even");
+    } else {
+        println!("Odd");
+    }
 }
