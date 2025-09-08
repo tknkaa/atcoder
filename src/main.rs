472be91dbb3f546a9007bac56997a5598f744e7a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-08 09:05:27 +0800

    ABC081B - Shift only

--- src/main.rs
+++ src/main.rs
@@ -3,10 +3,30 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
-    let s: u32 = buffer.trim().parse().unwrap();
-    let s3 = s % 10;
-    let s2 = (s % 100 - s3) / 10;
-    let s1 = (s - 10 * s2 - s3) / 100;
-    let sum = s1 + s2 + s3;
-    println!("{}", sum);
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let mut a: Vec<u32> = buffer
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let mut count = 0;
+    while judge_and_divide(&a) != Err(()) {
+        a = judge_and_divide(&a).unwrap();
+        count += 1;
+    }
+    println!("{}", count);
+}
+
+fn judge_and_divide(nums: &Vec<u32>) -> Result<Vec<u32>, ()> {
+    let mut divided: Vec<u32> = Vec::new();
+    for num in nums {
+        if num % 2 == 0 {
+            let divided_ref = &mut divided;
+            divided_ref.push(num / 2);
+        } else {
+            return Err(());
+        }
+    }
+    return Ok(divided);
 }
