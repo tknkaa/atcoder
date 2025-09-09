a81f22d2c0d9d28ccbddc49fb0cc404d436f295a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-09 15:47:41 +0800

    Kagami Mochi

--- src/main.rs
+++ src/main.rs
@@ -3,25 +3,16 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
+    let n: u32 = buffer.trim().parse().unwrap();
     buffer.clear();
-    io::stdin().read_line(&mut buffer).unwrap();
-    let mut cards: Vec<u32> = buffer
-        .trim()
-        .split_whitespace()
-        .map(|card| card.parse().unwrap())
-        .collect();
-    cards.sort();
-    let mut alice_score = 0;
-    let mut bob_score = 0;
-    let mut turn = 0;
-    while cards.len() > 0 {
-        if turn % 2 == 0 {
-            alice_score += cards.pop().unwrap();
-        } else {
-            bob_score += cards.pop().unwrap();
-        }
-        turn += 1;
+    let mut diameters: Vec<u32> = Vec::new();
+    for _i in 0..n {
+        io::stdin().read_line(&mut buffer).unwrap();
+        let d: u32 = buffer.trim().parse().unwrap();
+        diameters.push(d);
+        buffer.clear();
     }
-    let ans = alice_score - bob_score;
-    println!("{ans}");
+    diameters.sort();
+    diameters.dedup();
+    println!("{}", diameters.len());
 }
