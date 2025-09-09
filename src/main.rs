e132884ef5d00e4567cfc33ae5490f2738164998
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-09 16:54:51 +0800

    ABC085C - Otoshidama

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,22 @@ use std::io;
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
-    let n: u32 = buffer.trim().parse().unwrap();
-    buffer.clear();
-    let mut diameters: Vec<u32> = Vec::new();
-    for _i in 0..n {
-        io::stdin().read_line(&mut buffer).unwrap();
-        let d: u32 = buffer.trim().parse().unwrap();
-        diameters.push(d);
-        buffer.clear();
+    let combi: Vec<u32> = buffer
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let n = combi[0];
+    let y = combi[1];
+    for i in 0..(n + 1) {
+        for j in 0..(n + 1 - i) {
+            let k = n - i - j;
+            let total = 10000 * i + 5000 * j + 1000 * k;
+            if total == y {
+                println!("{} {} {}", i, j, k);
+                return;
+            }
+        }
     }
-    diameters.sort();
-    diameters.dedup();
-    println!("{}", diameters.len());
+    println!("-1 -1 -1");
 }
