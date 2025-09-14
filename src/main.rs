e6bb534586b5d2f0fb08aeebfece7842f8ca6ed5
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-09-14 11:19:30 +0800

    wip: 001 - Yokan Party

--- src/main.rs
+++ src/main.rs
@@ -1,62 +1,57 @@
 use std::io;
 
-#[derive(Debug)]
-struct Point {
-    t: u32,
-    x: u32,
-    y: u32,
-}
-
 fn main() {
     let mut buffer = String::new();
     io::stdin().read_line(&mut buffer).unwrap();
-    let n: u32 = buffer.trim().parse().unwrap();
+    let n_l: Vec<u32> = buffer
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let _n = *n_l.get(0).unwrap();
+    let l = *n_l.get(1).unwrap();
     buffer.clear();
-    let mut plan: Vec<Point> = Vec::new();
-    for _i in 0..n {
-        io::stdin().read_line(&mut buffer).unwrap();
-        let new_point: Vec<u32> = buffer
-            .trim()
-            .split_whitespace()
-            .map(|num| num.parse().unwrap())
-            .collect();
-        plan.push(Point {
-            t: new_point[0],
-            x: new_point[1],
-            y: new_point[2],
-        });
-        buffer.clear();
-    }
-    plan.insert(0, Point { t: 0, x: 0, y: 0 });
-    // if difference between t_k and t_{k+1} is odd, the distance should be odd?
-    // also, the distance should be less than or equal to the time distance?
-    for i in 0..n {
-        let i = i as usize;
-        let distance = measure_distance(&plan[i], &plan[i + 1]);
-        let interval = plan[i + 1].t - plan[i].t;
-        if distance > interval {
-            println!("No");
-            return;
-        }
-        if (plan[i + 1].t - plan[i].t) % 2 == 0 {
-            if distance % 2 == 0 {
-                continue;
-            } else {
-                println!("No");
-                return;
-            }
-        } else {
-            if distance % 2 == 1 {
-                continue;
-            } else {
-                println!("No");
-                return;
-            }
-        }
-    }
-    println!("Yes");
+    io::stdin().read_line(&mut buffer).unwrap();
+    let k: u32 = buffer.trim().parse().unwrap();
+    buffer.clear();
+    io::stdin().read_line(&mut buffer).unwrap();
+    let a: Vec<u32> = buffer
+        .trim()
+        .split_whitespace()
+        .map(|num| num.parse().unwrap())
+        .collect();
+    let score = search_score(&a, l, k);
+    println!("{score}");
 }
 
-fn measure_distance(point1: &Point, point2: &Point) -> u32 {
-    return point1.x.abs_diff(point2.x) + point1.y.abs_diff(point2.y);
+fn divide_k_plus_one_pieces_longer_than_m(a: &Vec<u32>, l: u32, k: u32, m: u32) -> bool {
+    let mut tmp: u32 = 0;
+    let mut count: u32 = 0;
+    for a_i in a {
+        if a_i - tmp >= m {
+            tmp = *a_i;
+            count += 1;
+        }
+    }
+    if count >= k + 1 && l - tmp >= m {
+        true
+    } else {
+        false
+    }
+}
+
+fn search_score(a: &Vec<u32>, l: u32, k: u32) -> u32 {
+    let mut start = 0;
+    let mut end = l;
+    while start <= end {
+        println!("{start} {end}");
+        let mid = (end - start) / 2;
+        println!("{}", divide_k_plus_one_pieces_longer_than_m(a, l, k, mid));
+        if divide_k_plus_one_pieces_longer_than_m(a, l, k, mid) {
+            start = mid;
+        } else {
+            end = mid;
+        }
+    }
+    start
 }
