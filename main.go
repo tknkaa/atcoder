4246007a6a52bb37833c965b0211d78fcb4f8b68
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-27 09:44:28 +0900

    b

--- main.go
+++ main.go
@@ -2,19 +2,62 @@ package main
 
 import (
 	"fmt"
-	"strings"
 )
+
+type masu struct {
+	done  bool
+	value int
+}
 
 func main() {
 	var n int
 	fmt.Scanf("%d", &n)
-	var s string
-	fmt.Scanf("%s", &s)
-	missing := n - len(s)
-	var ans strings.Builder
-	for _ = range missing {
-		ans.WriteString("o")
+	field := make([][]masu, n)
+	for i := range n {
+		field[i] = make([]masu, n)
 	}
-	ans.WriteString(s)
-	fmt.Println(ans.String())
+	r := 0
+	c := (n - 1) / 2
+	k := 1
+	field[r][c] = masu{
+		done:  true,
+		value: k,
+	}
+	for _ = range n*n - 1 {
+		if !field[mod(r-1, n)][mod(c+1, n)].done {
+			field[mod(r-1, n)][mod(c+1, n)] = masu{
+				done:  true,
+				value: k + 1,
+			}
+			r = mod(r-1, n)
+			c = mod(c+1, n)
+			k += 1
+		} else {
+			field[mod(r+1, n)][c] = masu{
+				done:  true,
+				value: k + 1,
+			}
+			r = mod(r+1, n)
+			k += 1
+		}
+	}
+	for i := range n {
+		for j := range n {
+			fmt.Print(field[i][j].value)
+			if j != n-1 {
+				fmt.Print(" ")
+			}
+		}
+		fmt.Printf("\n")
+	}
+}
+
+func mod(x, n int) int {
+	if x < 0 {
+		for x < 0 {
+			m := (-x) / n
+			x += n * (m + 1)
+		}
+	}
+	return x % n
 }
