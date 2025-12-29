b199383e349649875ab0ccd4564b8c34b8aae7e0
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-29 20:51:11 +0900

    A07

--- main.go
+++ main.go
@@ -10,28 +10,27 @@ import (
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
-
 	next := func() int {
 		scanner.Scan()
 		v, _ := strconv.Atoi(scanner.Text())
 		return v
 	}
-
+	d := next()
 	n := next()
-	q := next()
-
-	sum := make([]int, n+1)
-	for i := 1; i <= n; i++ {
-		val := next()
-		sum[i] = sum[i-1] + val
-	}
-
-	out := bufio.NewWriter(os.Stdout)
-	defer out.Flush()
-
-	for i := 0; i < q; i++ {
+	diff := make([]int, d+2)
+	for i := 0; i < n; i++ {
 		l := next()
 		r := next()
-		fmt.Fprintln(out, sum[r]-sum[l-1])
+		diff[l] += 1
+		diff[r+1] -= 1
+	}
+	sum := make([]int, d+2)
+	for i := 1; i <= d; i++ {
+		sum[i] = sum[i-1] + diff[i]
+	}
+	out := bufio.NewWriter(os.Stdout)
+	defer out.Flush()
+	for i := 1; i <= d; i++ {
+		fmt.Fprintln(out, sum[i])
 	}
 }
