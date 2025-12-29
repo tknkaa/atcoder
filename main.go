34b2a3230ff3c823a8746b99b20d081442e9ea52
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-29 21:58:37 +0900

    A09

--- main.go
+++ main.go
@@ -15,43 +15,41 @@ func main() {
 		v, _ := strconv.Atoi(scanner.Text())
 		return v
 	}
-	h := next()
-	w := next()
-	field := make([][]int, h+1)
+	h, w, n := next(), next(), next()
+	raw := make([][]int, h+2)
+	sum := make([][]int, h+2)
+	for i := 0; i <= h+1; i++ {
+		raw[i] = make([]int, w+2)
+		sum[i] = make([]int, w+2)
+	}
+
+	for i := 1; i <= n; i++ {
+		a, b, c, d := next(), next(), next(), next()
+		raw[a][b] += 1
+		raw[a][d+1] -= 1
+		raw[c+1][b] -= 1
+		raw[c+1][d+1] += 1
+	}
 	for i := 1; i <= h; i++ {
-		field[i] = make([]int, w+1)
 		for j := 1; j <= w; j++ {
-			field[i][j] = next()
+			sum[i][j] = sum[i][j-1] + raw[i][j]
 		}
 	}
 
-	sum0 := make([][]int, h+1)
-	for i := 1; i <= h; i++ {
-		sum0[i] = make([]int, w+1)
-		for j := 1; j <= w; j++ {
-			sum0[i][j] = sum0[i][j-1] + field[i][j]
-		}
-	}
-
-	sum1 := make([][]int, h+1)
-	for i := 0; i <= h; i++ {
-		sum1[i] = make([]int, w+1)
-	}
 	for i := 1; i <= h; i++ {
 		for j := 1; j <= w; j++ {
-			sum1[i][j] = sum1[i-1][j] + sum0[i][j]
+			sum[i][j] += sum[i-1][j]
 		}
 	}
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-
-	q := next()
-	for _ = range q {
-		a := next()
-		b := next()
-		c := next()
-		d := next()
-		sum := sum1[c][d] - sum1[c][b-1] - sum1[a-1][d] + sum1[a-1][b-1]
-		fmt.Fprintln(out, sum)
+	for i := 1; i <= h; i++ {
+		for j := 1; j <= w; j++ {
+			fmt.Fprint(out, sum[i][j])
+			if j != w {
+				fmt.Fprintf(out, " ")
+			}
+		}
+		fmt.Fprintf(out, "\n")
 	}
 }
