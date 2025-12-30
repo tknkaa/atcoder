a2a63e5ebfb419a22f753567f7a8e77652838e4e
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2025-12-30 17:49:52 +0900

    A11

--- main.go
+++ main.go
@@ -7,6 +7,10 @@ import (
 	"strconv"
 )
 
+func question(a, x int) bool {
+	return a >= x
+}
+
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
 	scanner.Split(bufio.ScanWords)
@@ -15,41 +19,22 @@ func main() {
 		v, _ := strconv.Atoi(scanner.Text())
 		return v
 	}
-	h, w, n := next(), next(), next()
-	raw := make([][]int, h+2)
-	sum := make([][]int, h+2)
-	for i := 0; i <= h+1; i++ {
-		raw[i] = make([]int, w+2)
-		sum[i] = make([]int, w+2)
-	}
-
+	n, x := next(), next()
+	a := make([]int, n+1)
 	for i := 1; i <= n; i++ {
-		a, b, c, d := next(), next(), next(), next()
-		raw[a][b] += 1
-		raw[a][d+1] -= 1
-		raw[c+1][b] -= 1
-		raw[c+1][d+1] += 1
+		a[i] = next()
 	}
-	for i := 1; i <= h; i++ {
-		for j := 1; j <= w; j++ {
-			sum[i][j] = sum[i][j-1] + raw[i][j]
-		}
-	}
-
-	for i := 1; i <= h; i++ {
-		for j := 1; j <= w; j++ {
-			sum[i][j] += sum[i-1][j]
+	left := -1
+	right := n
+	for right-left > 1 {
+		mid := (left + right) / 2
+		if question(a[mid], x) {
+			right = mid
+		} else {
+			left = mid
 		}
 	}
 	out := bufio.NewWriter(os.Stdout)
 	defer out.Flush()
-	for i := 1; i <= h; i++ {
-		for j := 1; j <= w; j++ {
-			fmt.Fprint(out, sum[i][j])
-			if j != w {
-				fmt.Fprintf(out, " ")
-			}
-		}
-		fmt.Fprintf(out, "\n")
-	}
+	fmt.Fprintln(out, right)
 }
