a029f2a5f72678d6cb37ba5701734aa32f8db900
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-08 15:37:08 +0800

    feat: implement digit summation for a three-digit integer

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
-    a = int(input())
-    [b, c] = list(map(int, input().split()))
-    s = input()
-    print(a + b + c, " ", s)
+    s = int(input())
+    s1 = int(s / 100)
+    s2 = int(s / 10 - s1 * 10)
+    s3 = int(s - s1 * 100 - s2 * 10)
+    print(s1 + s2 + s3)
 
 
 if __name__ == "__main__":
