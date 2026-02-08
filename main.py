8cddd342bdfc4927cff6a517a2894425cbd699c6
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-08 15:27:30 +0800

    chore: setup nix development environment and update main.py boilerplate

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 def main():
-    print("Hello from atcoder!")
+    a = int(input())
+    [b, c] = list(map(int, input().split()))
+    s = input()
+    print(a + b + c, " ", s)
 
 
 if __name__ == "__main__":
