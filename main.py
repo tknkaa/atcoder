d8ca99e6d7f1e25b4a2a4506da45dc87ef977544
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-09 23:20:41 +0800

    sake or water

--- main.py
+++ main.py
@@ -1,40 +1,21 @@
 def main():
-    n = int(input())
-    # initial state
-    t = [0]
-    x = [0]
-    y = [0]
-    for _ in range(0, n):
-        [a, b, c] = list(map(int, input().split(" ")))
-        t.append(a)
-        x.append(b)
-        y.append(c)
-
-    # length of states should be n + 1
-    for i in range(0, n):
-        x0 = x[i]
-        x1 = x[i + 1]
-        y0 = y[i]
-        y1 = y[i + 1]
-        t0 = t[i]
-        t1 = t[i + 1]
-        if judge(t0, t1, dist(x0, x1, y0, y1)):
-            continue
-        else:
-            print("No")
+    # n: cups, k: sake, x: minimum of sake
+    [n, k, x] = list(map(int, input().split(" ")))
+    # a_i: amount of liquid
+    a = list(map(int, input().split(" ")))
+    a.sort(reverse=True)
+    # suppose big cups are filled with water(n - k)
+    # 0, 0, 0,... ? > ? > ? ...
+    for i in range(0, n - k):
+        a[i] = 0
+    # sum up
+    sum = 0
+    for i, a_i in enumerate(a):
+        sum += a_i
+        if sum >= x:
+            print(i + 1)
             return
-    print("Yes")
-
-
-def dist(x0, x1, y0, y1):
-    return abs(x0 - x1) + abs(y0 - y1)
-
-
-def judge(t0, t1, dist):
-    lag = t1 - t0
-    if lag - dist >= 0 and (lag - dist) % 2 == 0:
-        return True
-    return False
+    print(-1)
 
 
 if __name__ == "__main__":
