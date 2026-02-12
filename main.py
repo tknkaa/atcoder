29ff1398600738f02e9263d5349aa44702bd8c8c
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-12 22:27:42 +0800

    feat: rewrite logic to process queries using binary search for integer calculations

--- main.py
+++ main.py
@@ -2,33 +2,57 @@ from typing import List
 
 
 def main():
-    t = int(input())
-    min_costs = [0] * t
-    for i in range(0, t):
-        [n, w] = list(map(int, input().split(" ")))
-        c = list(map(int, input().split(" ")))
-        min_costs[i] = min_cost(n, w, c)
-    for cost in min_costs:
-        print(cost)
+    [_, q] = list(map(int, input().split(" ")))
+    a_tmp = list(map(int, input().split(" ")))
+    a_tmp.sort()
+    inf = 10**9 + 1
+    a = [-inf]
+    a.extend(a_tmp)
+    a.append(inf)
+    answers = []
+    for _ in range(0, q):
+        [x, y] = list(map(int, input().split(" ")))
+        ans = calc_ans(a, x, y)
+        answers.append(ans)
+    for answer in answers:
+        print(answer)
 
 
-def min_cost(n: int, w: int, c: List[int]) -> int:
-    circle = [0] * (2 * w)
-    cycles = int(n / (2 * w))
-    for x in range(0, 2 * w):
-        for i in range(0, cycles + 1):
-            if x + i * 2 * w >= n:
-                continue
-            else:
-                circle[x] += c[x + i * 2 * w]
-    circle = circle * 3
-    sums = [0] * (2 * w)
+def find_s(a: List[int], x: int) -> int:
+    ng = -1
+    ok = len(a)
+    while ok - ng > 1:
+        mid = (ok + ng) // 2
+        if a[mid] >= x:
+            ok = mid
+        else:
+            ng = mid
+    return ok
 
-    current_sum = sum(circle[0:w])
-    for start in range(0, 2 * w):
-        current_sum = current_sum - circle[start] + circle[start + w]
-        sums[start] = current_sum
-    return min(sums)
+
+def find_t(a: List[int], x: int, y: int) -> int:
+    s = find_s(a, x)
+    ng = s - 1
+    ok = len(a)
+    while ok - ng > 1:
+        mid = (ok + ng) // 2
+        v = (a[mid] - x + 1) - (mid - s + 1)
+        if v >= y:
+            ok = mid
+        else:
+            ng = mid
+
+    if ok == len(a):
+        return ok - 1
+    else:
+        return ok
+
+
+def calc_ans(a: List[int], x: int, y: int) -> int:
+    s = find_s(a, x)
+    t = find_t(a, x, y)
+    ans = x + (y - 1) + (t - s)
+    return ans
 
 
 if __name__ == "__main__":
