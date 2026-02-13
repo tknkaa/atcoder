aae35dfe6a86b053c312619dad236d78ec474d2a
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-13 09:44:17 +0800

    tmp

--- main.py
+++ main.py
@@ -1,51 +1,32 @@
-from typing import List
-from bisect import bisect_left
+import math
 
 
 def main():
-    [_, q] = list(map(int, input().split(" ")))
-    a_tmp = list(map(int, input().split(" ")))
-    a_tmp.sort()
-    inf = 10**9 + 1
-    a = [-inf]
-    a.extend(a_tmp)
-    a.append(inf)
-    answers = []
-    for _ in range(0, q):
-        [x, y] = list(map(int, input().split(" ")))
-        ans = calc_ans(a, x, y)
-        answers.append(ans)
-    for answer in answers:
-        print(answer)
+    n = int(input())
+    max = int(math.sqrt(n))
+    ans = []
+    for i in range(1, max + 1):
+        for j in range(i + 1, max + 1):
+            sum = i**2 + j**2
+            if sum <= n:
+                ans.append(sum)
 
-
-def find_s(a: List[int], x: int) -> int:
-    return bisect_left(a, x)
-
-
-def find_t(a: List[int], x: int, y: int) -> int:
-    s = find_s(a, x)
-    ng = s - 1
-    ok = len(a)
-    while ok - ng > 1:
-        mid = (ok + ng) // 2
-        v = (a[mid] - x + 1) - (mid - s + 1)
-        if v >= y:
-            ok = mid
+    ans.sort()
+    check_list = []
+    duplicates = []
+    for a in ans:
+        if a not in check_list:
+            check_list.append(a)
         else:
-            ng = mid
+            duplicates.append(a)
 
-    if ok == len(a):
-        return ok - 1
-    else:
-        return ok
+    ans = [a for a in ans if a not in duplicates]
 
-
-def calc_ans(a: List[int], x: int, y: int) -> int:
-    s = find_s(a, x)
-    t = find_t(a, x, y)
-    ans = x + (y - 1) + (t - s)
-    return ans
+    print(len(ans))
+    ans_str = ""
+    for a in ans:
+        ans_str = ans_str + str(a) + " "
+    print(ans_str.strip())
 
 
 if __name__ == "__main__":
