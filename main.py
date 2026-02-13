6d031e161d435d1ebc21444155f8cd46b735e873
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-13 10:19:50 +0800

    refactor: optimize duplicate detection and output formatting using sets

--- main.py
+++ main.py
@@ -4,29 +4,21 @@ import math
 def main():
     n = int(input())
     max = int(math.sqrt(n))
-    ans = []
+    seen_once = set()
+    duplicates = set()
     for i in range(1, max + 1):
         for j in range(i + 1, max + 1):
             sum = i**2 + j**2
             if sum <= n:
-                ans.append(sum)
+                if sum in seen_once:
+                    duplicates.add(sum)
+                else:
+                    seen_once.add(sum)
 
-    ans.sort()
-    check_list = []
-    duplicates = []
-    for a in ans:
-        if a not in check_list:
-            check_list.append(a)
-        else:
-            duplicates.append(a)
-
-    ans = [a for a in ans if a not in duplicates]
+    ans = sorted(seen_once - duplicates)
 
     print(len(ans))
-    ans_str = ""
-    for a in ans:
-        ans_str = ans_str + str(a) + " "
-    print(ans_str.strip())
+    print(" ".join(map(str, ans)))
 
 
 if __name__ == "__main__":
