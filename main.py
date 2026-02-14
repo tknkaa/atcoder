94e479ab4e790f9e99ae592e40d18b9b86464180
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-14 18:23:05 +0800

    tmp

--- main.py
+++ main.py
@@ -1,21 +1,26 @@
+from typing import List
+
+
 def main():
-    n = int(input())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    c = list(map(int, input().split()))
-    d1 = [0] * n
-    d2 = [0] * n
-    d3 = [0] * n
-    for i in range(0, n):
-        if i == 0:
-            d1[i] = a[i]
-        else:
-            d1[i] = d1[i - 1] + a[i]
-    for i in range(1, n):
-        d2[i] = max([d1[i - 1], d2[i - 1]]) + b[i]
-    for i in range(2, n):
-        d3[i] = max([d2[i - 1], d3[i - 1]]) + c[i]
-    print(d3[n - 1])
+    t = int(input())
+    answers: List[int] = []
+    for _ in range(0, t):
+        n = int(input())
+        w: List[int] = []
+        p: List[int] = []
+        for _ in range(0, n):
+            [w_i, p_i] = list(map(int, input().split()))
+            w.append(w_i)
+            p.append(p_i)
+        answer = solve(w, p)
+        answers.append(answer)
+
+    for answer in answers:
+        print(answer)
+
+
+def solve(w: List[int], p: List[int]) -> int:
+    return 0
 
 
 if __name__ == "__main__":
