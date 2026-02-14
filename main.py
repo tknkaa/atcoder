64edce443966d762d36131d1e70f9500dbfd2d32
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-14 17:46:14 +0800

    feat: replace stack-based logic with dynamic programming implementation

--- main.py
+++ main.py
@@ -1,23 +1,21 @@
-from typing import List, Tuple
-
-
 def main():
-    _ = int(input())
+    n = int(input())
     a = list(map(int, input().split()))
-    q: List[int] = []
-    for a_i in a:
-        q.append(a_i)
-        delete_tail(q)
-
-    print(len(q))
-
-
-def delete_tail(q: List[int]) -> None:
-    if len(q) < 4:
-        return
-    if q[len(q) - 4] == q[len(q) - 3] == q[len(q) - 2] == q[len(q) - 1]:
-        for _ in range(4):
-            q.pop()
+    b = list(map(int, input().split()))
+    c = list(map(int, input().split()))
+    d1 = [0] * n
+    d2 = [0] * n
+    d3 = [0] * n
+    for i in range(0, n):
+        if i == 0:
+            d1[i] = a[i]
+        else:
+            d1[i] = d1[i - 1] + a[i]
+    for i in range(1, n):
+        d2[i] = max([d1[i - 1], d2[i - 1]]) + b[i]
+    for i in range(2, n):
+        d3[i] = max([d2[i - 1], d3[i - 1]]) + c[i]
+    print(d3[n - 1])
 
 
 if __name__ == "__main__":
