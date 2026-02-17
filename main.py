8493ac879a8fb1a4ad976ba06c8b47d83f22d601
tknkaa <tanaka-ryohei@g.ecc.u-tokyo.ac.jp>
2026-02-17 16:44:07 +0800

    feat: Implement solution for block placement

--- main.py
+++ main.py
@@ -1,37 +1,22 @@
-from typing import List
+from typing import Set, List
 
 
 def main():
-    n, m = map(int, input().split())
-    a = list(map(int, input().split()))
-    b = list(map(int, input().split()))
-    c = 998244353
-    a.sort()
+    [_, m] = list(map(int, input().split()))
+    blocks: Set[tuple] = set()
+    count = 0
 
-    # Prefix sum: prefix[i] = sum of a[0..i-1]
-    prefix = [0] * (n + 1)
-    for i in range(n):
-        prefix[i + 1] = prefix[i] + a[i]
-
-    result = 0
-    for j in range(m):
-        x = find_x(b[j], a)
-        result += (b[j] * x - prefix[x]) % c
-        result += ((prefix[n] - prefix[x]) - b[j] * (n - x)) % c
-    result %= c
-    print(result)
-
-
-def find_x(b_j: int, a: List[int]) -> int:
-    ng = -1
-    ok = len(a)
-    while ok - ng > 1:
-        mid = (ok + ng) // 2
-        if a[mid] >= b_j:
-            ok = mid
-        else:
-            ng = mid
-    return ok
+    for _ in range(0, m):
+        [r, c] = list(map(int, input().split()))
+        ok = True
+        for i in range(-1, 2):
+            for j in range(-1, 2):
+                if (r + i, c + j) in blocks:
+                    ok = False
+        if ok:
+            blocks.add((r, c))
+            count += 1
+    print(count)
 
 
 if __name__ == "__main__":
